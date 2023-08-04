dc183c18ae4e4b282e0eb2e6bbe01b72fbfffa7d
Ashish Singh <ashish.singh24785@gmail.com>
2023-08-04 21:18:20 +0530

    updating

--- Image slider/js/app.js
+++ Image slider/js/app.js
@@ -1,4 +1,32 @@
-let slides = document.querySelector(".slide");
+let slides = document.querySelectorAll(".slide");
+let counter = 0;
+console.log(slides);
 slides.forEach((slide,index) => {
-    console.log(slide);
+    slide.style.left = `${index * 100}%`
 })
+
+console.log(slides.length)
+
+let slideImg = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    })
+}
+
+let goNext = () => {
+    counter++;
+    if (counter >= slides.length){
+        counter = 0;
+    }
+    slideImg();
+}
+
+let goPrev = () => {
+    counter--;
+    if(counter < 0){
+        
+        counter = slides.length - 1;
+    }
+    slideImg();
+
+}
